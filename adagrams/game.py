dce961cc3afebc7e910097a4db835c9ad1eea61f
Gitika Kalal <gitikakalal21@gmail.com>
2025-03-18 21:02:16 -0700

    implement 'score_word' function and complet Wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -59,7 +59,48 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    score_chart = {
+        'A': 1, 
+        'E': 1, 
+        'I': 1, 
+        'O': 1, 
+        'U': 1, 
+        'L': 1, 
+        'N': 1, 
+        'R': 1, 
+        'S': 1, 
+        'T': 1, 
+        'D': 2, 
+        'G': 2, 
+        'B': 3, 
+        'C': 3, 
+        'M': 3, 
+        'P': 3, 
+        'F': 4, 
+        'H': 4, 
+        'V': 4, 
+        'W': 4, 
+        'Y': 4, 
+        'K': 5, 
+        'J': 8, 
+        'X': 8, 
+        'q': 10, 
+        'Z': 10
+    }
+    score = 0
+    length = len(word)
+
+    if length == 0:
+        return score
+    
+    if length > 6 and length < 11:
+        score += 8
+
+    for character in word:
+        character = character.upper()
+        score += score_chart[character]
+
+    return score
 
 def get_highest_word_score(word_list):
     pass
